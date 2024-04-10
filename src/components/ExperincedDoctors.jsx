b5786825f01692b9ccaaaccf5fdcbeb110fe4e37
antrikshkukreja01 <antrikshkukreja01@gmail.com>
2024-04-10 22:45:58 +0530

    first commit

---
+++ src/components/ExperincedDoctors.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const ExperincedDoctors = () => {
+  return (
+    <>
+      <section className="py-5">
+        <Container>
+          <h2 className=" mb-5">
+            Meet Our <span>Experienced Doctors</span>
+          </h2>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default ExperincedDoctors;
